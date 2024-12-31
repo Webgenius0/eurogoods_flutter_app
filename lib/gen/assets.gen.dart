467614896f7fc67f102de7586370f26324d4bbd7
Azizul Hakim <aziz.swe71@gmail.com>
2024-12-31 18:09:41 +0600

    change password, help center ui done

--- lib/gen/assets.gen.dart
+++ lib/gen/assets.gen.dart
@@ -54,8 +54,14 @@ class $AssetsIconsGen {
   /// File path: assets/icons/email.svg
   String get email => 'assets/icons/email.svg';
 
+  /// File path: assets/icons/emails.svg
+  String get emails => 'assets/icons/emails.svg';
+
   /// File path: assets/icons/eye.svg
   String get eye => 'assets/icons/eye.svg';
+
+  /// File path: assets/icons/facebook.svg
+  String get facebook => 'assets/icons/facebook.svg';
 
   /// File path: assets/icons/favorite.svg
   String get favorite => 'assets/icons/favorite.svg';
@@ -80,6 +86,9 @@ class $AssetsIconsGen {
 
   /// File path: assets/icons/imageforward.svg
   String get imageforward => 'assets/icons/imageforward.svg';
+
+  /// File path: assets/icons/instragram.svg
+  String get instragram => 'assets/icons/instragram.svg';
 
   /// File path: assets/icons/location.svg
   String get location => 'assets/icons/location.svg';
@@ -114,6 +123,9 @@ class $AssetsIconsGen {
   /// File path: assets/icons/payment.svg
   String get payment => 'assets/icons/payment.svg';
 
+  /// File path: assets/icons/phone.svg
+  String get phone => 'assets/icons/phone.svg';
+
   /// File path: assets/icons/plash_icon.svg
   String get plashIcon => 'assets/icons/plash_icon.svg';
 
@@ -131,6 +143,9 @@ class $AssetsIconsGen {
 
   /// File path: assets/icons/search_normal.svg
   String get searchNormal => 'assets/icons/search_normal.svg';
+
+  /// File path: assets/icons/twitter.svg
+  String get twitter => 'assets/icons/twitter.svg';
 
   /// File path: assets/icons/user.svg
   String get user => 'assets/icons/user.svg';
@@ -151,7 +166,9 @@ class $AssetsIconsGen {
         editProfile,
         editProfilePic,
         email,
+        emails,
         eye,
+        facebook,
         favorite,
         filter,
         filterBlack,
@@ -160,6 +177,7 @@ class $AssetsIconsGen {
         home,
         imagebackward,
         imageforward,
+        instragram,
         location,
         locationChange,
         logOut,
@@ -171,12 +189,14 @@ class $AssetsIconsGen {
         orderSuccess,
         password,
         payment,
+        phone,
         plashIcon,
         plus,
         plusWithBorder,
         profile,
         search,
         searchNormal,
+        twitter,
         user
       ];
 }
